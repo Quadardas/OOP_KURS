1dcee7e34670f6422cb7a518641da69ff9191f91
akladskiy <akladskiy@gmail.com>
2021-05-25 20:57:43 +0500

    отбеливание

---
+++ OOP_KURS/Cosmetic.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_KURS
+{
+    public class Cosmetic : Services
+    {
+        string CosmeticType;
+
+        public Cosmetic(string cosmeticType, string name, int price) : base(name, price)
+        {
+            this.CosmeticType = cosmeticType;
+        }
+        public string thisCosmeticType
+        {
+            get { return this.CosmeticType; }
+            set { this.CosmeticType = value is string ? value : null; }
+        }
+    }
+}
